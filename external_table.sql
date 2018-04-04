59508c0f38aeb18613db6650cee4cb66bcae030a
Saman Munikar <samanmunikar@gmail.com>
2018-04-04 09:34:20 +0545

    External_table.sql modified and partitions_ana_random_values.sql file added

--- external_table.sql
+++ external_table.sql
@@ -1,25 +1,30 @@
+---------------------------------------create a directory containing flat file--------------------------------------------------
 create or replace directory external_table as 'C:\Oracle\External' ;
 select * from dba_directories;
 
+-------------------------------------------Drop external table if any-----------------------------------------------------------
 drop table ext_table;
+
+-------------------------------------------Create an external Table-------------------------------------------------------------
 create table ext_table(
-id number,
-name varchar2(25),
-address varchar2(50)
+    id number,
+    name varchar2(25),
+    address varchar2(50)
 )
 ORGANIZATION EXTERNAL (
-TYPE ORACLE_LOADER
-DEFAULT DIRECTORY external_table
-Access parameters (
-RECORDS DELIMITED BY NEWLINE
-NOBADFILE
-NOLOGFILE
-NODISCARDFILE
-SKIP 1
-FIELDS TERMINATED BY '|'
+    TYPE ORACLE_LOADER
+    DEFAULT DIRECTORY external_table
+    Access parameters (
+    RECORDS DELIMITED BY NEWLINE
+    BADFILE bdump:'read_alert_%a_%p.bad'
+    LOGFILE bdump:'read_alert_%a_%p.log'
+    SKIP 1
+    FIELDS TERMINATED BY '|'
+    )
+    LOCATION ('sample_file.csv')
 )
-LOCATION (EXTERNAL_TABLE :'sample_file.csv')
-)
+PARALLEL
 REJECT LIMIT UNLIMITED;
 
+---------------------------------------------Select from external table---------------------------------------------------------
 select * from ext_table;
